87ae69ad9ab464c8b963025b4aa6f127dbc69828
cai_fedora <16326494+cai-zixiang_hainan@user.noreply.gitee.com>
2026-06-11 18:56:23 +0800

    set the Install as default.tar.get

---
+++ bin/clash.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sleep 2
+echo "try to start clash"
+date
+
+./clash -d .
+
+echo "script exec ok"
---
+++ bin/mktmp.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "waiting for make /tmp/cai for 3 s"
+
+sleep 3
+
+mkdir -p /tmp/cai
